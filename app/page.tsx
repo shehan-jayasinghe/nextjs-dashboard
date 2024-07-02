71d8673114294eabe6bae02c7bdec10fb7b0cb51
blackJs <89008575+shehan-jayasinghe@users.noreply.github.com>
2024-07-02 19:27:54 +0530

    Adding a primary font

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+      <div
+          className="relative shadow-gray-50 w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+      />
+  );
+}
