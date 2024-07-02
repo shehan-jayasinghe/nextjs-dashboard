cbd88c5ea922c7794938fc134e3917a025ee71f4
blackJs <89008575+shehan-jayasinghe@users.noreply.github.com>
2024-07-02 20:13:17 +0530

    feat: The <Image> component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import AcmeLogo from "@/app/ui/acme-logo";
+import Image from 'next/image';
 export default function Page() {
   return (
-      <div
-          className="relative shadow-gray-50 w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-      />
+      <main className="flex min-h-screen flex-col p-6">
+          <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+              <AcmeLogo/>
+          </div>
+          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+              <Image
+                  src="/hero-desktop.png"
+                  width={1000}
+                  height={760}
+                  className="hidden md:block"
+                  alt="Screenshots of the dashboard project showing desktop version"
+              />
+          </div>
+      </main>
   );
 }
