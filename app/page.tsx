7cd1614f8b2176d7f7d0b601378f232769909f4f
blackJs <89008575+shehan-jayasinghe@users.noreply.github.com>
2024-07-13 17:31:31 +0530

    feat: Navigating Between Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import Image from 'next/image';
+import NavLinks from "@/app/ui/dashboard/nav-links";
 export default function Page() {
   return (
       <main className="flex min-h-screen flex-col p-6">
+          <NavLinks/>
           <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
               <AcmeLogo/>
           </div>
